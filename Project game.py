3494d57a5320bf2275beef8cc5fbc4d430304cf5
NoopyPro <164927543+NoopyPro@users.noreply.github.com>
2024-07-13 19:03:40 +0200

    basics
    finished basic building blocks for code

---
+++ Project game.py
@@ -0,0 +1,14 @@
+# Imports modules.
+import functions
+
+
+# Code for attacking.
+def player_attack():
+    pass
+
+# Main game code.
+def main_game():
+    functions.print_pause_color("'You enter a room weilding nothing but your wit and a small knife.'","yellow")
+
+# Starts game.
+main_game()
---
+++ functions.py
@@ -0,0 +1,93 @@
+import time
+import random
+
+# ANSI color codes for foreground text colors
+class colors:
+    """ ANSI color codes """
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BROWN = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    MAGENTA = "\033[95m"
+    CYAN = "\033[96m"
+    WHITE = "\033[97m"
+    LIGHT_GRAY = "\033[0;37m"
+    DARK_GRAY = "\033[1;30m"
+    LIGHT_RED = "\033[1;31m"
+    LIGHT_GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
+    LIGHT_BLUE = "\033[1;34m"
+    LIGHT_PURPLE = "\033[1;35m"
+    LIGHT_CYAN = "\033[1;36m"
+    LIGHT_WHITE = "\033[1;37m"
+    BOLD = "\033[1m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    NEGATIVE = "\033[7m"
+    CROSSED = "\033[9m"
+    END = "\033[0m"  # Reset to default color
+
+def print_pause(string:str):
+    print("")
+    print(string)
+    time.sleep(1.6)
+
+def print_pause_color(string: str, color: str):
+    print("")
+    if color == "black":
+        print(colors.BLACK + string + colors.END)
+    elif color == "red":
+        print(colors.RED + string + colors.END)
+    elif color == "green":
+        print(colors.GREEN + string + colors.END)
+    elif color == "brown":
+        print(colors.BROWN + string + colors.END)
+    elif color == "blue":
+        print(colors.BLUE + string + colors.END)
+    elif color == "purple":
+        print(colors.PURPLE + string + colors.END)
+    elif color == "magenta":
+        print(colors.MAGENTA + string + colors.END)
+    elif color == "cyan":
+        print(colors.CYAN + string + colors.END)
+    elif color == "white":
+        print(colors.WHITE + string + colors.END)
+    elif color == "light_gray":
+        print(colors.LIGHT_GRAY + string + colors.END)
+    elif color == "dark_gray":
+        print(colors.DARK_GRAY + string + colors.END)
+    elif color == "light_red":
+        print(colors.LIGHT_RED + string + colors.END)
+    elif color == "light_green":
+        print(colors.LIGHT_GREEN + string + colors.END)
+    elif color == "yellow":
+        print(colors.YELLOW + string + colors.END)
+    elif color == "light_blue":
+        print(colors.LIGHT_BLUE + string + colors.END)
+    elif color == "light_purple":
+        print(colors.LIGHT_PURPLE + string + colors.END)
+    elif color == "light_cyan":
+        print(colors.LIGHT_CYAN + string + colors.END)
+    elif color == "light_white":
+        print(colors.LIGHT_WHITE + string + colors.END)
+    elif color == "bold":
+        print(colors.BOLD + string + colors.END)
+    elif color == "faint":
+        print(colors.FAINT + string + colors.END)
+    elif color == "italic":
+        print(colors.ITALIC + string + colors.END)
+    elif color == "underline":
+        print(colors.UNDERLINE + string + colors.END)
+    elif color == "blink":
+        print(colors.BLINK + string + colors.END)
+    elif color == "negative":
+        print(colors.NEGATIVE + string + colors.END)
+    elif color == "crossed":
+        print(colors.CROSSED + string + colors.END)
+    elif color == "reset":
+        print(string + colors.END)
+    time.sleep(1.6)
